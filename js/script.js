2c6560461fe1808fd87762542a33ea97bc49ee7b
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-09 23:41:35 +0200

    implementato else if sulle condizioni sconti

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
+// PREZZO FISSO PER KM
 const prezzo =(0,21);
 console.log(prezzo);
 
+// RACCOGLI INFO KM
 let distanza = prompt('quanti chilometri');
 console.log(distanza);
 
+// RACCOGLI INFO ANNI
 let età = prompt('quanti anni');
 console.log(età);
 
+// VARIABILI FASCE DI ETA
 let scontoOver45 = (45);
 let scontoUnder18 = (18);
 
-if (età >= scontoOver45) {
+// CALCOLO PREZZO PER KM
+
+// VARIABILE PREZZI
+
+
+// CONDIZIONI SCONTI
+if (età <=  scontoUnder18) {
+    console.log('scontoUnder18');
+} else if (età >= scontoOver45)
     console.log('scontoOver45');
-} 
- if (età <= scontoUnder18)
-     console.log('scontoUnder18');
+ 
 
