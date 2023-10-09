7928f9d6e03646636c2c1f1830a613214b606250
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-09 23:22:51 +0200

    aggiunto variabile sconti per età

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ console.log(distanza);
 
 let età = prompt('quanti anni');
 console.log(età);
+
+let scontoOver45 = (45);
+let scontoUnder18 = (18);
+
+if (età >= scontoOver45) {
+    console.log('scontoOver45');
+} 
+ if (età <= 18)
+     console.log('scontoUnder18');
+
